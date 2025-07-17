51bc38dfe948c96e4547e849e042eaa1aaf6b254
Tanishpreet Kaur <kaurtanishpreet320@gmail.com>
2025-07-17 12:02:47 +0530

    1darray

---
+++ Java/src/Array1d.java
@@ -0,0 +1,12 @@
+public class Array1d {
+    public static void main(String[] args) {
+        int[] marks = {10, 20, 30, 40, 50};
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(marks[i]);
+        }
+        //for each loop
+        //for(int num:marks){
+        //System.out.println(marks);
+    //}
+    }
+}
